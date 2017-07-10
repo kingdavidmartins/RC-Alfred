be42405c87b08b04455c722ff04d238da5e04d7c
King <kingdavidmartins@users.noreply.github.com>
2017-07-10 15:35:54 -0400

    basic API.AI webhook helloworld

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,24 @@
 'use strict';
 
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
 
-exports.helloFromKing = functions.https.onRequest((request, response) => {
- response.send("Hello from King David Martins!. The Recurse center is AWESOME!!! WHOOP WHOOP!!");
+// [START YourAction]
+exports.rcAction = functions.https.onRequest((request, response) => {
+  const app = new App({request, response});
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
+
+  // Fulfill action business logic
+  function responseHandler (app) {
+    // Complete your fulfillment logic and send a response
+    app.tell('Hello, World! From Rc');
+  }
+
+  const actionMap = new Map();
+  actionMap.set('input.welcome', responseHandler);
+
+  app.handleRequest(actionMap);
 });
+// [END YourAction]
