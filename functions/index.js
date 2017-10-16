c2708cb4cccf62a2ba71d9fac9f991f8326bd2bf
King <kingdavidmartins@users.noreply.github.com>
2017-10-15 23:18:21 -0400

    require fact type. refactor

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
+const fact = require('./rcFactType.js');
 
 // API.AI actions
 const UNRECOGNIZED_DEEP_LINK = 'deeplink.unknown';
@@ -16,43 +17,9 @@ const FACT_TYPE = {
   CULTURE: 'culture'
 };
 
-const HISTORY_FACTS = new Set([
-  'master wayne sir. Did you know on September 23, 2006 RC founder\'s Nick and Dave met for the first time during their signals and systems class? Well to be honest it wasn\'t on September 23. It was actually during Fall 2006. I was close enough though.',
-  'master wayne sir. Did you know in the summer of 2007 RC founder\'s Nick and Sonali met at shakespeare in the park. I\'m just here forever alone. No one wants to meet me or talk to me.',
-  'master wayne sir. Did you know in the spring of 2008 RC founder\'s Nick and Dave began having business meetings after work to discuss starting a company. And by company I mean RC. In case you were wondering. Even though you probably weren\'t. I just wanted to feel special.',
-  'master wayne sir. Did you know around May 2010. Nick and Dave quit their programming jobs to start OkCupid for jobs.',
-  'master wayne sir. Did you know from June to October 2010. Nick and Dave was living in Mountain View, CA and did Y Combinator. It was after they decided to leave that Paul Graham calls them "self-indulgent" for wanting to move back to New York.',
-  'master wayne sir. Did you know from Fall 2010. Sonali started working with Nick and Dave on weekends.',
-  'master wayne sir. Did you know on February 2011. Sonali ddecided to leave her cushy corporate job and salary to join the team full-time!',
-  'master wayne sir. Did you know on June 2011. Dave has a long talk with Paul Graham, Sam Altman and Paul Buchheit about a "school for hackers" and returns to New York ecstatic about the idea. They then invited a small group of people to be part of an experiment called "hacker school"',
-  'master wayne sir. Did you know on July 16, 2011. A third of the people who committed to doing the batch dropped out two days before the start',
-  'master wayne sir. Did you know on July 18, 2011. Was the first day of the summer 2011 batch. The batch only had nine people including Nick, Dave, and Sonali, and it was five weeks long.',
-  'master wayne sir. Did you know on August 2011. John Workman suggests "Never graduate" as Hacker School\'s informal motto',
-  'master wayne sir. Did you know on September 2011. The second batch begins. We\'re hosted at Spotify in a slightly larger room with no windows.',
-  'master wayne sir. Did you know on February 2012. The third batch begins with 20 Hacker Schoolers and Tom is among them. The batch is hosted at The Huffington Post, and Danielle Sucher becomes the first Hacker School alumna',
-  'master wayne sir. Did you know on May 2012. We hired Tom and Alan as contractors to help facilitate the summer batch',
-  'master wayne sir. Did you know on June 2012. The summer batch begins with 53 Hacker Schoolers, among them are Allison, Zach and Mary. The batch is 45% women, and is hosted at Etsy.',
-  'master wayne sir. Did you know on August 2012. Tom and Alan become regular employees and are joined by Zach and Allison.',
-  'master wayne sir. Did you know on October 2012. Jessica McKellar, Peter Seibel, Alex Payne, Stefan Karpinski, and David Nolen are the first Residents.',
-  'master wayne sir. Did you know on February 2013. We got our own space in Manhattan! The US government agrees that Mary is "extraordinary" and grants her a work visa',
-  'master wayne sir. Did you know on September 2013. We move to an nicer space with lots of natural light and no fruit flies.',
-  'master wayne sir. Did you know on May 2014. To encourage traditions to persist from batch to batch, we move to overlapping batches.',
-  'master wayne sir. Did you know on June 2014. We hire our first dedicated operations person, Rachel.',
-  'master wayne sir. Did you know on November 2014. Allison goes west to join Dropbox: An unusual goodbye.',
-  'master wayne sir. Did you know on March 2015. We change our name from Hacker School to the Recurse Center.',
-  'master wayne sir. Did you know on May 2015. Michael Nielsen joins the Recurse Center to help build a research lab.',
-  'master wayne sir. Did you know on August 2015. Allie (W\'13) and John (S\'11) join RC as facilitators; Nancy joins as our first full-time Jobs Counselor.',
-  'master wayne sir. Did you know on March 2016. Allie and John transition to our dev team. James (S\'13) and Lisa (S\'12) join RC as facilitators.',
-  'master wayne sir. Did you know on June 2016. Rose (W\'14) joins RC as a facilitator.',
-  'master wayne sir. Did you know on September 2016. Allie moves on to work at Etsy, and John starts a new job at Eave. Lisa moves west and joins Pluot.',
-  'master wayne sir. Did you know on February 2017. Rose leaves RC. James begins transitioning into a new role with more focus on making improvements to the Retreat.',
-  'master wayne sir. Did you know on March 2017. Zach moves out of New York, and becomes our first remote employee. We relaunch our website with a snazzy new design.'
-]);
+const HISTORY_FACTS = new Set(fact.history);
 
-const CULTURE_FACTS = new Set([
-  'master wayne sir. Did you know that RC is the place of dreams and butterflies. A place where everyone loves each other.',
-  'master wayne sir. Hold on give me a second. Batman needs my assistance. Sorry duty calls'
-]);
+const CULTURE_FACTS = new Set(fact.culture);
 
 const NEXT_FACT_DIRECTIVE = ' Would you like to hear another fact master wayne?';
 const CONFIRMATION_SUGGESTIONS = ['Sure', 'Thank you, bye'];
